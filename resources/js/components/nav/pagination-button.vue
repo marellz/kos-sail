9eb4d21730525623a8e8021160392659907c7845
Dave Njoroge <d.njoroge@thecodingmachine.com>
2024-08-21 14:16:42 +0300

    pagination fix for query links

--- resources/js/components/nav/pagination-button.vue
+++ resources/js/components/nav/pagination-button.vue
@@ -2,8 +2,11 @@
     <nav-link
         as="button"
         class="inline-flex space-x-2 border-l border-y last:border-r px-3 py-2 first:rounded-l-lg last:rounded-r-lg text-sm"
-        :class="{ 'text-primary bg-light-alt-primary': active,
-    'text-grey opacity-50': disabled }"
+        :class="{
+            'text-primary bg-light-alt-primary': active,
+            'text-grey opacity-50': disabled,
+        }"
+        :disabled
     >
         <slot />
     </nav-link>
--- resources/js/components/nav/pagination.vue
+++ resources/js/components/nav/pagination.vue
@@ -1,30 +1,19 @@
 <template>
     <div class="py-10">
         <nav class="flex items-center justify-center">
-            <pagination-button :disabled="!hasPrev" :href="hasPrev ? `${url}?page=${currentPage - 1}` : url">
-                <ArrowLeftCircleIcon class="h-4"/>
-                <span>Previous</span>
-            </pagination-button>
             <pagination-button
-                v-for="link in meta.last_page"
-                :href="`${url}?page=${link}`"
-                :key="`btn-page-${link}`"
-                :active="meta.current_page === link"
+                :disabled="link.url === null"
+                v-for="link in meta.links"
+                :href="link.url ?? '/'"
+                v-html="link.label"
+                :active="link.active"
             >
-                {{link}}
-            </pagination-button>
-             <pagination-button :disabled="!hasNext" :href="hasNext ? `${url}?page=${currentPage + 1}`: url">
-                <span>Next</span>
-                <ArrowRightCircleIcon class="h-4"/>
             </pagination-button>
         </nav>
     </div>
 </template>
 <script lang="ts" setup>
 import PaginationButton from "@/components/nav/pagination-button.vue";
-import { ArrowLeftCircleIcon, ArrowRightCircleIcon } from "@heroicons/vue/24/outline";
-import { usePage } from "@inertiajs/vue3";
-import { computed } from "vue";
 
 interface Link {
     label: string;
@@ -32,16 +21,11 @@ interface Link {
     active: boolean;
 }
 
-const props = defineProps<{
+defineProps<{
     meta: {
         links: Array<Link>;
         last_page: number;
         current_page: number;
     };
 }>();
-
-const url = computed(()=> usePage().props.ziggy.location)
-const hasNext = computed(() => props.meta.current_page < props.meta.last_page)
-const hasPrev = computed(() => props.meta.current_page > 1)
-const currentPage = computed(() => props.meta.current_page)
 </script>
