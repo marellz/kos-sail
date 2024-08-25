591f12fc62f05636fa87db9374c5cbc068ed36d8
Dave Njoroge <d.njoroge@thecodingmachine.com>
2024-08-25 14:41:22 +0300

    dashboard layout redesign

--- resources/js/layouts/dashboard.vue
+++ resources/js/layouts/dashboard.vue
@@ -1,93 +1,110 @@
 <template>
-    <div class="bg-lighter-alt text-dark min-h-screen">
-        <header class="py-6">
-            <layout-container class="flex justify-between">
-                <div class="flex items-center space-x-2">
-                    <button type="button" class="p-3 md:hidden" @click="toggleNav">
-                        <Bars2Icon class="h-5"/>
-                    </button>
-                    <nav-link href="/dashboard">
-                        <img src="@/assets/images/logo-md.svg" class="h-8" alt="" />
-                    </nav-link>
-                </div>
-                <div class="flex items-center">
-                    <!-- dark toggle -->
-
-                    <!-- user dropdown -->
-                    <dropdown>
-                        <template #trigger>
-                            <UserCircleIcon class="h-10 text-gray-300" />
-                            <div class="ml-2 hidden md:inline text-left leading-4 pt-1">
-                                <p class="font-medium">
-                                    {{ user.name }}
-                                </p>
-                                <p class="text-xs text-grey">Admin</p>
-                            </div>
-                            <span class="bg-light-grey rounded-full p-1">
-                                <ChevronDownIcon class="h-5" />
-                            </span>
-                        </template>
-                        <dropdown-link href="/logout" method="post" as="button"
-                            >Logout</dropdown-link
-                        >
-                    </dropdown>
-                </div>
-            </layout-container>
+    <div class="text-dark min-h-screen flex">
+       
             <!-- nav -->
-            <div ref="target" class="bg-white fixed top-0 w-full p-9 transform -translate-y-full transition md:static md:bg-transparent md:p-0 md:transform-none z-20" :class="{'!translate-y-0': navActive}"> 
-                <div class="flex justify-between text-light-grey md:hidden">
-                    <button type="button" @click="toggleNav">
+            <div
+                ref="target"
+                class="w-full sm:w-64 bg-gray-50 border-r flex-none fixed h-full top-0 -translate-x-full z-50 p-10 md:p-8 md:relative md:translate-x-0 md:h-auto transition"
+                :class="{ '!translate-x-0': navActive }"
+            >
+                <div class="flex justify-between text-light-grey">
+                    <button type="button" @click="toggleNav" class="md:hidden">
                         <ChevronLeftIcon class="h-6" />
                     </button>
                     <h1 class="uppercase font-semibold text-2xl">Menu</h1>
                 </div>
-                <div class="mt-10 border-b">
-                    <layout-container>
-                        <nav class="flex flex-col md:flex-row md:items-center md:space-x-5">
-                            <nav-link
-                                v-for="(link, index) in links"
-                                :key="index"
-                                :href="link.path"
-                                :active="link.components.includes($page.component)"
-                                class="inline-flex items-center space-x-2 p-2"
-                            >
-                                <component :is="link.icon" class="h-5" />
-                                <span>{{ link.label }}</span>
-                            </nav-link>
-                        </nav>
-                    </layout-container>
+                <div class="mt-10">
+                    <nav class="flex flex-col">
+                        <nav-link
+                            v-for="(link, index) in links"
+                            :key="index"
+                            :href="link.path"
+                            :active="link.components.includes($page.component)"
+                            class="inline-flex items-center space-x-2 p-2"
+                        >
+                            <component :is="link.icon" class="h-5" />
+                            <span>{{ link.label }}</span>
+                        </nav-link>
+                    </nav>
                 </div>
             </div>
-        </header>
-        <main class="py-8 overflow-auto">
-            <slot />
-        </main>
+            <div class="flex-auto h-screen overflow-auto">
+                <layout-container class="!px-0">
+                    <header class="py-6 flex sticky top-0 bg-white z-40 border-b">
+                        <div class="flex items-center space-x-2 flex-auto">
+                            <button
+                                type="button"
+                                class="p-3 md:hidden"
+                                @click="toggleNav"
+                            >
+                                <Bars2Icon class="h-5" />
+                            </button>
+                            <nav-link href="/dashboard">
+                                <img
+                                    src="@/assets/images/logo-md.svg"
+                                    class="h-8"
+                                    alt=""
+                                />
+                            </nav-link>
+                        </div>
+                        <div class="flex items-center">
+                            <!-- dark toggle -->
+    
+                            <!-- user dropdown -->
+                            <dropdown>
+                                <template #trigger>
+                                    <UserCircleIcon class="h-10 text-gray-300" />
+                                    <div
+                                        class="ml-2 hidden md:inline text-left leading-4 pt-1"
+                                    >
+                                        <p class="font-medium">
+                                            {{ user.name }}
+                                        </p>
+                                        <p class="text-xs text-grey">Admin</p>
+                                    </div>
+                                    <span class="bg-light-grey rounded-full p-1">
+                                        <ChevronDownIcon class="h-5" />
+                                    </span>
+                                </template>
+                                <dropdown-link
+                                    href="/logout"
+                                    method="post"
+                                    as="button"
+                                    >Logout</dropdown-link
+                                >
+                            </dropdown>
+                        </div>
+                    </header>
+                    <main class="py-8">
+                        <slot />
+                    </main>
+                </layout-container>
+            </div>
     </div>
 </template>
 <script lang="ts" setup>
 import { usePage } from "@inertiajs/vue3";
 import { UserCircleIcon, ChevronDownIcon } from "@heroicons/vue/24/solid";
 import {
-Bars2Icon,
+    Bars2Icon,
     ChevronLeftIcon,
     EnvelopeIcon,
     HomeIcon,
     QueueListIcon,
     Squares2X2Icon,
 } from "@heroicons/vue/24/outline";
-import { computed,ref } from "vue";
-import { onClickOutside } from '@vueuse/core'
-
+import { computed, ref } from "vue";
+import { onClickOutside } from "@vueuse/core";
 
 const { props } = usePage();
 
 const user = computed(() => props.auth.user);
 
-const navActive = ref(false)
+const navActive = ref(false);
 
 const toggleNav = () => {
-    navActive.value = !navActive.value
-}
+    navActive.value = !navActive.value;
+};
 
 const links = ref([
     {
@@ -100,7 +117,11 @@ const links = ref([
         path: "/dashboard/products",
         label: "Products",
         icon: Squares2X2Icon,
-        components: ["dashboard/products/index", "dashboard/products/show", "dashboard/products/form"],
+        components: [
+            "dashboard/products/index",
+            "dashboard/products/show",
+            "dashboard/products/form",
+        ],
     },
     {
         path: "/dashboard/categories",
@@ -116,10 +137,9 @@ const links = ref([
     },
 ]);
 
-
-const target = ref(null)
+const target = ref(null);
 
 onClickOutside(target, () => {
-    navActive.value = false
-})
+    navActive.value = false;
+});
 </script>
